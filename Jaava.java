dd170c7b0eece9c597b118a7585e0e237982ca2a
Sainath Dussa <126276841+DussaSainath2@users.noreply.github.com>
2025-06-06 19:55:26 +0530

    Update Jaava.java

--- Jaava.java
+++ Jaava.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Main{
   public static void main(String args[]){
-    System.out.println("Sainath Dussa");
+    System.out.println("Sainath Dussa power star");
   }
 }
